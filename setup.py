38fcc363acc03090943e912df0d9a17ad12a565a
heverton <heverton.sarah@gmail.com>
2017-04-01 12:55:52 -0300

    Adding files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='firstprojectr',
+    packages=['firstprojectr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
